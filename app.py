1729dea308aba9d778b99a014aa997bb1d6f00a8
OlexiyKi <olexiykirsanov@gmail.com>
2022-11-26 15:06:51 +0200

    empty funcsions

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def login_user():
+    if request.method == 'GET':
+        pass
+    else:
+        pass
+    return '<p>login!</p>'
+
+
+@app.route('/logout', methods=['GET'])
+def logout_user():
+    return '<p>logout!</p>'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    return '<p>registration form</p>'
+
+
+@app.route('/user_page', methods=['GET'])
+def user_access():
+    return 'more function'
+
+
+@app.route('/currency', methods=['GET', 'POST'])
+def currency_converter():
+    return 'currency_converter'
 
